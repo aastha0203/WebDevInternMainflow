7e20abe7f2f1a5b2e6b1855506c0ad7bc3fb3134
AASTHA GUPTA <100026195+aastha0203@users.noreply.github.com>
2024-06-24 17:54:28 +0530

    Implemented useState hook to manage task states ,Included functionality to add new tasks and persist them in localStorage, Implemented useEffect to load tasks from localStorage

--- Task-4/todo-app/src/App.js
+++ Task-4/todo-app/src/App.js
@@ -1,8 +1,33 @@
 import './App.css';
-import React,{useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import { AiOutlineDelete } from "react-icons/ai";
+import { BsCheckLg } from "react-icons/bs";
 
 function App() {
-  const [isCompleteScreen,setIsCompleteScreen]=useState(false)
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false);
+  const [allTodos,setTodos]=useState([]);
+  const [newTitle,setNewTitle]=useState("");
+  const [newDescription,setNewDescription]=useState("");
+
+  const handleAddTodo=()=>{
+    let newTodoItem={
+      title:newTitle,
+      description:newDescription
+    }
+    let updatedTodoArr=[...allTodos];
+    updatedTodoArr.push(newTodoItem);
+    setTodos(updatedTodoArr);
+    localStorage.setItem('todolist',JSON.stringify(updatedTodoArr))
+  };
+  
+  useEffect(()=>{
+      let savedTodo=JSON.parse(localStorage.getItem('todolist'))
+      if(savedTodo){
+        setTodos(savedTodo);
+      }
+  },[])
+
+
   return (
     <div className="App">
       <h1>My Todos</h1>
@@ -10,25 +35,36 @@ function App() {
         <div className="todo-input">
           <div className="todo-input-item">
             <label>Title</label>
-            <input type="text" placeholder="What's the task title?" />
+            <input type="text" value={newTitle} onChange={(e)=>setNewTitle(e.target.value)} placeholder="What's the task title?" />
           </div>
           <div className="todo-input-item">
             <label>Description</label>
-            <input type="text" placeholder="What's the task description?" />
+            <input type="text" value={newDescription} onChange={(e)=>setNewDescription(e.target.value)}placeholder="What's the task description?" />
           </div>
           <div className="todo-input-item">
-           <button type="button" className="primaryBtn">Add</button>
+            <button type="button" onClick={handleAddTodo}className="primaryBtn">Add</button>
           </div>
         </div>
         <div className="btn-area">
-          <button className={`secondaryBtn ${isCompleteScreen===false && 'active'}`}onClick={()=>setIsCompleteScreen(false)}>ToDo</button>
-          <button className={`secondaryBtn ${isCompleteScreen===true && 'active'}`}onClick={()=>setIsCompleteScreen(true)}>Completed</button>
+          <button className={`secondaryBtn ${isCompleteScreen === false && 'active'}`} onClick={() => setIsCompleteScreen(false)}>ToDo</button>
+          <button className={`secondaryBtn ${isCompleteScreen === true && 'active'}`} onClick={() => setIsCompleteScreen(true)}>Completed</button>
         </div>
         <div className="todo-list">
-          <div className="todo-list-item">
-            <h3>Task 1</h3>
-            <p>Description</p>
+          {allTodos.map((item,index)=>{
+            return(
+            <div className="todo-list-item" key={index}>
+            <div>
+              <h3>{item.title}</h3>
+              <p>{item.description}</p>
+            </div>
+            <div>
+              <AiOutlineDelete className='icon' />
+              <BsCheckLg className='check-icon' />
+            </div>
           </div>
+            )
+          })}
+
 
         </div>
       </div>
