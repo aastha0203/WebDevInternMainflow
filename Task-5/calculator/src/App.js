ebe753a1c5486ae32b2a0e58e39a113108fa1df4
AASTHA GUPTA <100026195+aastha0203@users.noreply.github.com>
2024-06-27 14:12:04 +0530

    added css for styling and implemented calculator logic and error handling

--- Task-5/calculator/src/App.js
+++ Task-5/calculator/src/App.js
@@ -5,7 +5,68 @@ const App = () => {
   const [input, setInput] = useState('0');
   const [result, setResult] = useState('');
   const handleButtonClick = (value) => {
-  
+    if (value === '=') {
+      try {
+        setResult(evaluateExpression(input));
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'R') {
+      setInput('0');
+      setResult('');
+    } else if (value === 'D') {
+      setInput((prevInput) =>
+        prevInput.length > 1 ? prevInput.slice(0, -1) : '0'
+      );
+    } else {
+      setInput((prevInput) => (prevInput === '0' ? value : prevInput + value));
+    }
+  };
+  const evaluateExpression = (expression) => {
+    try {
+      let tokens = expression.match(/(\d+(\.\d+)?|[+\-*\/])/g);
+      if (!tokens) throw new Error("Invalid expression");
+
+      let stack = [];
+      let num = '';
+      let lastOp = '+';
+
+      const applyOp = (operator, num) => {
+        switch (operator) {
+          case '+':
+            stack.push(num);
+            break;
+          case '-':
+            stack.push(-num);
+            break;
+          case '*':
+            stack.push(stack.pop() * num);
+            break;
+          case '/':
+            if (num === 0) throw new Error("Division by zero");
+            stack.push(stack.pop() / num);
+            break;
+          default:
+            break;
+        }
+      };
+
+      for (let token of tokens) {
+        if (!isNaN(token)) {
+          num = parseFloat(token);
+          applyOp(lastOp, num);
+        } else {
+          lastOp = token;
+        }
+      }
+
+      let result = stack.reduce((a, b) => a + b, 0);
+      if (isNaN(result)) throw new Error("Invalid calculation");
+
+      return parseFloat(result.toFixed(10)).toString();
+    } catch (error) {
+      return 'Error';
+    }
   };
 
 
