b5acae0de8574a77ac371e0b0e2f8045a01cfec1
AASTHA GUPTA <100026195+aastha0203@users.noreply.github.com>
2024-06-27 13:46:39 +0530

    added button component and event handling

--- Task-5/calculator/src/App.js
+++ Task-5/calculator/src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 const App = () => {
   const [input, setInput] = useState('0');
   const [result, setResult] = useState('');
+  const handleButtonClick = (value) => {
+  
+  };
+
 
   return (
     <div className="calculator">
@@ -12,6 +16,11 @@ const App = () => {
         <div className="result">{result}</div>
       </div>
       <div className="buttons">
+      {['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+', 'D', 'R'].map((btn) => (
+          <button key={btn} onClick={() => handleButtonClick(btn)}>
+            {btn}
+          </button>
+        ))}
       </div>
     </div>
   );
